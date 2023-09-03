aaa592e8e38d81c172b25132035c0701e8efd772
SSA <frash-nn95@mail.ru>
2023-09-03 18:03:50 +0300

    Сделал задачу 64 в task25

---
+++ task25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число от 1 и более");
+int num = int.Parse(Console.ReadLine()!);
+
+string NumMin(int n){
+    if (n > 0){
+        return n + " "+ NumMin(n-1);
+    }
+    else{
+        return "";
+    }
+}
+
+Console.WriteLine(NumMin(num));
